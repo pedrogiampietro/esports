78b7b6ed8a95660f3fa3524963e6ad47b6e9c512
Pedro Giampietro <pedrogiampietro@hotmail.com>
2022-10-11 21:56:55 -0300

    ajusts app.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar, View } from 'react-native';
 import { Background } from './src/components/Background';
-
+// 2, 1:28:11
 export default function App() {
   return (
     <Background>
