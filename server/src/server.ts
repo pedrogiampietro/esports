871fe1283b050427bc5d61433700936edde9a8ac
pedrogiampietro <pedrogiampietro@hotmail.com>
2022-10-19 15:19:59 -0300

    add: done backend.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,97 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+import cors from 'cors'
+import {
+	convertHourStringToMinutes,
+	convertMinutesToHourString,
+} from './utils/convertTime'
 
 const app = express()
+app.use(cors())
+app.use(express.json())
 
-app.get('/ads', (req, res) => {})
+const prisma = new PrismaClient()
+
+app.get('/games', async (request, response) => {
+	const games = await prisma.game.findMany({
+		include: {
+			_count: {
+				select: {
+					ads: true,
+				},
+			},
+		},
+	})
+
+	return response.json(games)
+})
+
+app.get('/games/:id/ads', async (request, response) => {
+	const gameId = request.params.id
+	const ads = await prisma.ad.findMany({
+		select: {
+			id: true,
+			name: true,
+			weekDays: true,
+			useVoiceChannel: true,
+			yearsPlaying: true,
+			hourStart: true,
+			hourEnd: true,
+		},
+		where: {
+			gameId,
+		},
+		orderBy: {
+			createdAt: 'desc',
+		},
+	})
+	return response.json(
+		ads.map((ad: any) => {
+			return {
+				...ad,
+				weekDays: ad.weekDays.split(','),
+				hourStart: convertMinutesToHourString(ad.hourStart),
+				hourEnd: convertMinutesToHourString(ad.hourEnd),
+			}
+		})
+	)
+})
+
+app.get('/ads/:id/discord', async (request, response) => {
+	const adId = request.params.id
+
+	const ad = await prisma.ad.findUniqueOrThrow({
+		select: {
+			discord: true,
+		},
+		where: {
+			id: adId,
+		},
+	})
+
+	return response.json({
+		discord: ad.discord,
+	})
+})
+
+app.post('/games/:id/ads', async (request, response) => {
+	const gameId = request.params.id
+	const body = request.body
+
+	const ad = await prisma.ad.create({
+		data: {
+			gameId,
+			name: body.name,
+			yearsPlaying: body.yearsPlaying,
+			discord: body.discord,
+			weekDays: body.weekDays.join(','),
+			hourStart: convertHourStringToMinutes(body.hourStart),
+			hourEnd: convertHourStringToMinutes(body.hourEnd),
+			useVoiceChannel: body.useVoiceChannel,
+		},
+	})
+
+	return response.json(ad)
+})
 
 app.listen(3333)
